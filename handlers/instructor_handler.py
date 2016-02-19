5a3a84587b29efc2819c0513388af8894bc6e889
Alex Tsankov <antsankov@gmail.com>
2016-02-18 19:24:34 -0700

    Added in new rendering for instructor_info

--- handlers/instructor_handler.py
+++ handlers/instructor_handler.py
@@ -3,4 +3,15 @@ import tornado.template
 
 class InstructorHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.render('layouts/instructor_view.html')
+		instructor_info_object = {
+			"first_name" : "John",
+			"last_name" : "Black",
+			"department" : "Computer Science",
+			"type" : "TTT",
+			"sections" : 12,
+			"courses" : 4,
+			"last_fcq" : "Spring 2015",
+			"first_fcq" : "Spring 2012",
+		}
+
+		self.render('layouts/instructor_view.html',instructor_info=instructor_info_object)
