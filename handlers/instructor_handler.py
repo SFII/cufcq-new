20c252afbd801bc7c4d8cf91f3fa803934006d9b
Alex Tsankov <antsankov@gmail.com>
2016-02-29 19:42:56 -0700

    added in fcqs

--- handlers/instructor_handler.py
+++ handlers/instructor_handler.py
@@ -15,10 +15,41 @@ class InstructorHandler(tornado.web.RequestHandler):
 		}
 		instructor_stats_object = {
 			"effectiveness" : 5,
-			"overall" : 5,
-			"availability" : 5,
-			"respect" : 5,
+			"overall" : 3,
+			"availability" : 6,
+			"respect" : 2,
 		}
+
+		department_info_object = {
+			"name" : "Computer Science",
+			"num_ugrads" : 556,
+			"num_grads" : 43,
+			"num_courses" : 5,
+			"average_workload" : "4hrs",
+			# NOT TAs,
+			"num_instructors" : 58,
+			"first_fcq" : "Spring 2012",
+		}
+		class1 = {
+			"name" : "Spring 2015 ACCT 5220-2 Playing with Dildos",
+			"info" : "All you need to know!",
+			"sdfjksjkdf" : 'akjhsdakljsdjklasd',
+		}
+		class2 = {
+			"name" : "Fall 2014 PSYCH 1300-2 Amateur Freud",
+			"info" : "Facts out the wazoo!",
+			"sdfjksjkdf" : 'akjhsdakljsdjklasd',
+		}
+		class3 = {
+			"name" : "Fall 2013 CSCI 5001-2 Making Moneyzz",
+			"info" : "Daddy Warbucks!",
+			"sdfjksjkdf" : 'akjhsdakljsdjklasd',
+		}
+		instructor_fcqs_array = [
+		class1,class2,class3
+		]
 		self.render('layouts/instructor_view.html',
 			instructor_info=instructor_info_object,
-			instructor_stats=instructor_stats_object)
+			instructor_stats=instructor_stats_object,
+			department_info=department_info_object,
+			instructor_fcqs=instructor_fcqs_array)
