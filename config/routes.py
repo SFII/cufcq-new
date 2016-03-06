ff7c30d442a3d815abbbe72ffc345018b16f463b
Alex Tsankov <antsankov@gmail.com>
2016-03-06 11:06:23 -0700

    sidebar

--- config/routes.py
+++ config/routes.py
@@ -3,7 +3,7 @@ Routing configuration.
 """
 from handlers.index_handler import IndexHandler
 from handlers.instructor_handler import InstructorHandler
-# from handlers.course_handler import CourseHandler
+from handlers.course_handler import CourseHandler
 # from handlers.department_handler import DepartmentHandler
 from handlers.api_handler import ApiHandler
 # Tornado pro-tip: regex routing is optimized by putting more frequently
@@ -11,7 +11,7 @@ from handlers.api_handler import ApiHandler
 routes = [
     (r"/", IndexHandler),
     (r"/instructors", InstructorHandler),
-    # (r"/courses", CourseHandler),
+    (r"/courses", CourseHandler),
     # (r"/departments", DepartmentHandler),
     # (r"/instructor/([\w-]+)", InstructorHandler),
     (r"/api/(instructor|course|fcq|department)/([\w-]+)", ApiHandler)
