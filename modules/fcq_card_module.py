f34b3763a7c2527f3f7f5dc6f08b0820bc8c4a1c
untra <untra.sam@gmail.com>
2016-03-16 15:46:56 -0600

    onetime onclick ajax

--- modules/fcq_card_module.py
+++ modules/fcq_card_module.py
@@ -7,36 +7,37 @@ class FcqCardModule(BaseModule):
 
     def render(self, fcq_id):
         self.fcq_ids.append(fcq_id)
+
         return '''
         <div class="card panel">
             <div class="card-head card-head-sm collapsed" data-toggle="collapse" data-parent="#{0}" data-target="#{0}" aria-expanded="false">
-                <header id="header-{0}">{0}</header>
+                <header id="header-{0}">{1}</header>
                 <div class="tools">
                     <a class="btn btn-icon-toggle"><i class="fa fa-angle-down"></i></a>
                 </div>
             </div>
             <div id="{0}" class="collapse" aria-expanded="false" style="height: 0px;">
-              <div id="body-{0}" class="card-body"><p>{0}</p>
+              <div id="body-{0}" class="card-body"><p><i>loading...</i></p>
               </div>
             </div>
         </div>
-        '''.format(fcq_id)
+        '''.format(fcq_id, self.fcq_title(fcq_id))
 
     def embedded_javascript(self):
         javascript = ""
         for fcq_id in self.fcq_ids:
             javascript += '''
-            $.ajax({{
-                type: "GET",
-                url: "/api/fcq/{0}",
-                success: function(data, status) {{
-                    console.log(data);
-                    $("#body-{0}").html("<i>wieners</i>");
-                }},
-                error: function() {{
-                    console.log("AJAX ERROR: {0} could not be loaded.");
-                }}
+            $("#header-{0}").one( "click", function(){{
+                $.ajax({{
+                    type: "GET",
+                    url: "/api/fcq/{0}",
+                    success: function(data, status) {{
+                        $("#body-{0}").html(JSON.stringify(data));
+                    }},
+                    error: function() {{
+                        console.log("AJAX ERROR: {0} could not be loaded.");
+                    }}
+                }})
             }});
-            console.log("{0}");
             '''.format(fcq_id)
         return javascript
